5773f1cc792653104d0c9bc7205e2f4f56fc420b
dariameln1 <dariamelnykova1@gmail.com>
2023-12-17 18:36:57 +0100

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,34 @@
-import { GlobalStyle } from './GlobalStyle';
-import { Component } from "react";
+import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { FilterField } from './FilterField/Filter';
+import { GlobalStyle } from './GlobalStyle';
+
+
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 const LOCALSTORAGE = 'contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-  };
+const getSavedContacts = () => {
+  const savedContacts = window.localStorage.getItem(LOCALSTORAGE);
+  return savedContacts !== null ? JSON.parse(savedContacts) : defaultContacts;
+};
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem(LOCALSTORAGE);
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(getSavedContacts);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem(LOCALSTORAGE, JSON.stringify(contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem(LOCALSTORAGE, JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const addContact = newContact => {
     const isDuplicate = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
@@ -43,65 +36,39 @@ export class App extends Component {
     if (isDuplicate) {
       alert(`${newContact.name} is already in contacts.`);
       return;
-    };
+    }
 
-    const contact = {
-      ...newContact,
-      id: nanoid(),
-    };
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
-  }
-  // - Filter -
-  onFilter = value => {
-    this.setState({
-      filter: value,
-    });
+    setContacts(prevContacts => [
+      ...prevContacts,
+      {
+        ...newContact,
+        id: nanoid(),
+      },
+    ]);
   };
 
-  getFilterdContacts = () => {
-    return this.state.contacts.filter(contact => {
-      return contact.name
-        .toLowerCase()
-        .includes(this.state.filter.toLowerCase());
-    });
+  const getFilteredContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <h2>Contacts</h2>
+      <FilterField filter={filter} onChange={setFilter} />
+      {contacts.length > 0 && (
+        <ContactsList contacts={getFilteredContacts()} onDelete={deleteContact} />
+      )}
 
-  // - Delete -
-  
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
-  };
-  
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filterdContacts = this.getFilterdContacts();
-
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <ContactForm onAdd={this.addContact} />
-        <h2>Contacts</h2>
-        <FilterField filter={filter} onChange={this.onFilter} />
-        {contacts.length > 0 && (
-          <ContactsList
-            contacts={filterdContacts}
-            onDelete={this.deleteContact}
-          />
-        )}
-        
-        <GlobalStyle/>
-      </>
-    )
-  }
-}
+      <GlobalStyle />
+    </>
+  );
+};
